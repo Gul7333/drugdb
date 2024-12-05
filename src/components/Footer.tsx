107284377919c742caf081945aa35430ed7b90bb
Gul7333 <g.task.pk@gmail.com>
2024-12-05 16:39:08 +0500

    chn

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="mt-12 text-center text-gray-500">
+    <footer className="mt-12 text-center text-gray-600">
     <p>&copy; 2024 Drug Database. All rights reserved.</p>
   </footer>  )
 }
